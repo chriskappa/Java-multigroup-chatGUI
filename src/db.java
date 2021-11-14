0df4cda439b1fd667711621595c449c3e2b3be60
chriskappa <chriskagkelis@yahoo.com>
2021-11-14 16:25:04 +0000

    Added Register Screen

--- src/db.java
+++ src/db.java
@@ -10,6 +10,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
@@ -47,6 +48,36 @@ public class db {
         }
         return null;
     }
+   
+   public void registerUser(String userName,String Password,String email){
+       try {
+            Connection con = getConnection();
+       PreparedStatement ps;
+       ResultSet rs;
+       String query = " insert into users ( userName, userPassword  , userEmail)"
+        + " values ( ?, ?, ? )";
+       ps=con.prepareStatement(query);
+//       ps.setInt(1, 3);
+       ps.setString(1, userName);
+       ps.setString(2, Password);
+       ps.setString(3, email);
+        ps.execute();
+           System.out.println("User Registered");
+       } catch (SQLException e) {
+           String[] msg = e.getMessage().split(" ");
+           if(msg[0].equals("Duplicate")){
+               System.out.println("Please Change Your Email, Its Already Exist!");
+           }
+           else{
+           System.out.println("Couldnt Register User");
+           System.out.println(e.toString());
+           }
+           
+       }
+       
+      
+       
+   }
 
 
 
